888da7c27fb53b6b83fbff2a6a6b2bbcd73439f9
angelinaanaki <a.anakievskaa@gmail.com>
2023-11-15 08:49:33 +0100

    [16.0][ADD] stock_quant_package_fast_move

---
+++ stock_quant_package_fast_move/models/res_company.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2023 Cetmix OÜ
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    package_move_picking_type_id = fields.Many2one(
+        "stock.picking.type", string="Package Move Operation"
+    )
---
+++ stock_quant_package_fast_move/readme/CONFIGURE.rst
@@ -0,0 +1,5 @@
+- Go to the "Inventory -> Configuration -> Settings"
+- Activate the "Packages" checkbox
+- In the "Package Move Operation" select the operation type that will be used for the easy package transfers. This setting is configured for each company separately
+
+.. image:: ../static/img/package_fast_move_configure.png
