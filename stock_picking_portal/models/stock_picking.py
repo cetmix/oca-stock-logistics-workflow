fba028c503b02588b84caee2edb93324ee3e6225
dessanhemrayev <dessanhemrayev@gmail.com>
2024-05-12 23:15:59 +0300

    [ADD] stock_picking_portal: add module
    This module allows to show stock operations in the customer portal.
    Users have access to the following stock operations:
    
    Operation type is available in portal
    User (partner) is set as Customer
    
    Task: 3612.

---
+++ stock_picking_portal/models/stock_picking.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2024 Cetmix OÜ
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class StockPick(models.Model):
+    _name = "stock.picking"
+    _inherit = ["portal.mixin", "stock.picking"]
+
+    signed_by = fields.Char(copy=False)
+    signed_on = fields.Datetime(copy=False)
+
+    @api.model
+    def _get_available_operations(self):
+        return (
+            self.env["stock.picking.type"].search([("portal_visible", "=", True)]).ids
+        )
+
+    def _compute_access_url(self):
+        super()._compute_access_url()
+        for picking in self:
+            picking.access_url = "/my/stock_operations/%s" % (picking.id)
+        return
+
+    def _get_report_base_filename(self):
+        self.ensure_one()
+        return "%s %s" % (self.picking_type_id.name, self.name)
